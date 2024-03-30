3333e1cb052fe42cf242ebd5fa582b8896507c73
Ajay Kumar <124868129+ajay-kumar-iphtech@users.noreply.github.com>
2024-03-30 11:54:34 +0530

    add grid

--- Appsinvo/ViewController.swift
+++ Appsinvo/ViewController.swift
@@ -11,13 +11,35 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var myCollectionView: UICollectionView!
+    @IBOutlet weak var gridCollectionView: UICollectionView!
     
-
+    var screenSize: CGRect!
+    var screenWidth: CGFloat!
+    var screenHeight: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
+        screenSize = UIScreen.main.bounds
+        screenWidth = screenSize.width
+        screenHeight = screenSize.height
+        
+        let gridLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+            gridLayout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 10, right: 0)
+            let cellWidth = (screenWidth - 20) / 4 // Adjust spacing and width as needed
+            gridLayout.itemSize = CGSize(width: cellWidth, height: cellWidth)
+            gridLayout.minimumInteritemSpacing = 0 // Adjust spacing between items
+            gridLayout.minimumLineSpacing = 0 // Adjust spacing between rows
+            gridCollectionView.collectionViewLayout = gridLayout
+        
         myCollectionView.delegate = self
         myCollectionView.dataSource = self
+        
+        gridCollectionView.delegate = self
+        gridCollectionView.dataSource = self
+        
     }
 
 
@@ -25,21 +47,55 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        // Return the number of items based on the collection view
+        if collectionView == myCollectionView {
+            return 3 // Number of items for myCollectionView
+        } else if collectionView == gridCollectionView {
+            return 9 // Number of items for gridCollectionView
+        }
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // Configure and return the appropriate cell based on the collection view
         
-        let cell = myCollectionView.dequeueReusableCell(withReuseIdentifier: "myCollectionViewCell", for: indexPath) as! myCollectionViewCell
-        return cell
+        if collectionView == myCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCollectionViewCell", for: indexPath) as! myCollectionViewCell
+            // Configure myCollectionViewCell
+            return cell
+        } else if collectionView == gridCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "gridCollectionViewCell", for: indexPath) as! gridCollectionViewCell
+            // Configure gridCollectionViewCell
+            return cell
+        }
+        
+        // Default return
+        return UICollectionViewCell()
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // Return the size for items based on the collection view
+        
+        if collectionView == myCollectionView {
+            // Return size for myCollectionView
+            return CGSize(width: collectionView.bounds.width, height: 500)
+        } else if collectionView == gridCollectionView {
+            // Return size for gridCollectionView
+            return CGSize(width: gridCollectionView.bounds.width / 3, height: gridCollectionView.bounds.width / 3)
+        }
+        
+        // Default return
+        return CGSize.zero
+    }
     
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//    
-//           return CGSize(width: collectionView.bounds.width, height: 500)
-//       }
-    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = gridCollectionView.dequeueReusableCell(withReuseIdentifier: "gridCollectionViewCell", for: indexPath as IndexPath) as! gridCollectionViewCell
+            cell.layer.borderWidth = 0.5
+            cell.frame.size.width = screenWidth / 3
+            cell.frame.size.height = screenWidth / 3
+
+            return cell
+        }
 }
+
---
+++ Appsinvo/gridCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  gridCollectionViewCell.swift
+//  Appsinvo
+//
+//  Created by Ajay Kumar on 30/03/24.
+//
+
+import UIKit
+
+class gridCollectionViewCell: UICollectionViewCell {
+    
+}
